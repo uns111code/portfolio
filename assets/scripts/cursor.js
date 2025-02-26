bca6afe7e56693ef3c240ea021a5ee2e441894dc
uns.code <uns.code111@gmail.com>
2025-02-26 22:43:27 +0100

    cursor

---
+++ assets/scripts/cursor.js
@@ -0,0 +1,26 @@
+document.addEventListener("mousemove", function(event) {
+    let trace = document.createElement("div");
+    trace.classList.add("trace");
+    
+    let x = event.clientX;
+    let y = event.clientY;
+
+    trace.style.left = `${x}px`;
+    trace.style.top = `${y}px`;
+
+    document.body.appendChild(trace);
+
+    // Supprime la traînée après un certain temps pour éviter les ralentissements
+    setTimeout(() => {
+        trace.remove();
+    }, 500);
+});
+
+
+
+
+ // Couleur aléatoire
+    // let colors = ["#ff00ff", "#00ffff", "#ffff00", "#ff0000", "#00ff00", "#0000ff"];
+    // let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    // trace.style.boxShadow = `0 0 10px ${randomColor}, 0 0 20px ${randomColor}, 0 0 30px ${randomColor}`;
+    // trace.style.background = randomColor;
